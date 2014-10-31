2fb2b361db599cd32abecf6dd6711487ca66b3d7
Keith Ensign <keithensign@Keiths-iMac.local>
2014-10-31 15:26:43 -0700

    continue building out Galleries.

---
+++ static/js/3fo/Gallery.js
@@ -0,0 +1,27 @@
+
+$(function() {
+	if (typeof TFO === 'undefined') {
+		TFO = {};
+	}
+
+	TFO.__GalleryInstance = function () {
+
+	};
+
+
+	TFO.__GalleryInstance.prototype.init = function() {
+
+	};
+
+	TFO.__GalleryInstance.prototype.populateThumbsRow = function() {
+
+	};
+
+	TFO.__GalleryInstance.prototype.shiftThumbs = function(){
+
+	};
+
+
+	//create instance
+	TFO.Gallery = new TFO.__GalleryInstance();
+});
