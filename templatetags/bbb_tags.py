93b598f28201d5bffca1a6d0dd3df9c93f432ddb
Keith Ensign <keithensign@Keiths-iMac.local>
2014-11-03 15:28:29 -0800

    get tickets tag now accepts variables for links and target.  Added the global footer GET TICKETS link

--- templatetags/bbb_tags.py
+++ templatetags/bbb_tags.py
@@ -4,7 +4,7 @@ from mezzanine.galleries.models import Gallery
 
 register = template.Library()
 
-@register.inclusion_tag('includes/gallery_images.html')
+@register.inclusion_tag('includes/tags/gallery_images.html')
 def get_gallery(name):
     gallery = Gallery.objects.filter(slug=name)
     Gallery.objects.filter()
@@ -12,6 +12,10 @@ def get_gallery(name):
     return {"gallery": gallery}
 
 
-@register.inclusion_tag('includes/bbb/get-tickets-button.html')
-def get_tickets_button():
-    return
+@register.inclusion_tag('includes/tags/get-tickets-button.html')
+def get_tickets_button(link, target=None):
+    return_target = False
+    if target is not None:
+        return_target = target
+
+    return {"link": link, "target": return_target}
