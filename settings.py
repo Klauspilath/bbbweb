ea438d3acf745464e1496f97f11089bee1414896
Keith Ensign <kensign@decisionpatterns.com>
2014-11-12 14:25:20 -0800

    added max file size

--- settings.py
+++ settings.py
@@ -166,6 +166,8 @@ ADMINS = (
 )
 MANAGERS = ADMINS
 
+MAX_UPLOAD_SIZE = "5242880"
+
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = []
