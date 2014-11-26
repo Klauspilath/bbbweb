6013ff55ee28dba36d051a4359b94d51d4ac6bd5
Keith Ensign <kensign@300feetout.com>
2014-11-25 17:09:05 -0800

    cast page now shows the header image correctly, resolved the broken drop down on the index page.

--- static/js/3fo/Page.js
+++ static/js/3fo/Page.js
@@ -1,67 +1,65 @@
-$(function() {
+$(function () {
 
-	if (typeof TFO === 'undefined') {
-		TFO = {};
-	}
+    if (typeof TFO === 'undefined') {
+        TFO = {};
+    }
 
-	TFO.__PageInstance = function () {
-		this.initializeResizeListener();
-		this.setNavigationEvents();
-		this.init = true;
+    TFO.__PageInstance = function () {
+        this.initializeResizeListener();
+        this.setNavigationEvents();
+        this.init = true;
 
-	};
+    };
 
-	TFO.__PageInstance.prototype.initializeResizeListener = function () {
-		$(window).resize(function(){
-			TFO.Page.setHeroImageHeight()
-		});
-	};
+    TFO.__PageInstance.prototype.initializeResizeListener = function () {
+        $(window).resize(function () {
+            TFO.Page.setHeroImageHeight()
+        });
+    };
 
-	TFO.__PageInstance.prototype.setHeroImageHeight = function () {
-		var hi = $("#hero-image"),
-			pt = $("section > div:nth-child(1) > h1"),
-			nv = $("nav"),
-			hd = $("header");
+    TFO.__PageInstance.prototype.setHeroImageHeight = function () {
+        var hi = $("#hero-image"),
+            pt = $("section > div:nth-child(1) > h1"),
+            nv = $("nav"),
+            hd = $("header");
 
-		var titleHeight = pt.height();
-			if(!TFO.Page.init){
-				titleHeight = 106;
-				TFO.Page.init = false;
-			}
+        var titleHeight = pt.height();
+        if (!TFO.Page.init) {
+            titleHeight = 106;
+            TFO.Page.init = false;
+        }
 
-		hi.css("height", $(window).height()
-			- (titleHeight
-			+ nv.height()
-			+ hd.height())
-		);
-	};
+        hi.css("height", $(window).height()
+                - (titleHeight
+                    + nv.height()
+                    + hd.height())
+        );
+    };
 
+    TFO.__PageInstance.prototype.setNavigationEvents = function () {
+        var $dropdowns = $('li.dropdown');
 
-	TFO.__PageInstance.prototype.setNavigationEvents = function(){
-		var $dropdowns = $('li.dropdown');
+        // Mouseenter (used with .hover()) does not trigger when user enters from outside document window
 
-		// Mouseenter (used with .hover()) does not trigger when user enters from outside document window
-
-		$dropdowns.on('mouseenter', function(){
-			var sc = $('#screen');
-			var bg = $(this).children(".nav-background");
-			if(sc.offset().left == bg.offset().left) return;
-        	var bgl = sc.offset().left -  bg.offset().left;
-			bg.css("left", bgl + "px");
+        $dropdowns.on('mouseenter', function () {
+            var sc = $('#screen');
+            var bg = $(this).children(".nav-background");
+            if (sc.offset().left == bg.offset().left) return;
+            var bgl = sc.offset().left - bg.offset().left;
+            bg.css("left", bgl + "px");
         })
-	};
+    };
 
-	TFO.__PageInstance.prototype.getState = function(){
-		return this.state;
-	};
+    TFO.__PageInstance.prototype.getState = function () {
+        return this.state;
+    };
 
-	TFO.__PageInstance.prototype.setState = function(state){
-		this.state = state;
-	};
+    TFO.__PageInstance.prototype.setState = function (state) {
+        this.state = state;
+    };
 
-	//create instance
-	if(!TFO.globals.constants.IS_HOME){
-		TFO.Page = new TFO.__PageInstance();
-		$(window).trigger('resize');
-	}
+    TFO.Page = new TFO.__PageInstance();
+    if (!TFO.globals.constants.IS_HOME) {
+        $(window).trigger('resize');
+    }
 });
