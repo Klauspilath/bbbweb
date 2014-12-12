17df0a40828f40dd2fc18d39639ffd5e56b0f4aa
Keith Ensign <kensign@300feetout.com>
2014-12-12 10:10:46 -0800

    cast and creative font sizes and overlay fixes

--- static/js/3fo/Cast.js
+++ static/js/3fo/Cast.js
@@ -8,27 +8,30 @@ $(function() {
 	};
 
 	TFO.__CastInstance.prototype.initOverlays = function(){
-		var container = $(".headshot");
 
-		container.on('mouseenter',
-			function (event) {
-				var overlay = $(event.currentTarget).children(".member-overlay");
-				overlay.css("display", "block");
-				overlay.toggleClass('red-semi-trans');
-				$(event.currentTarget).children('.cast-member-name').toggleClass('red-semi-trans');
-			}
-		);
+		if( !TFO.globals.constants.IS_MOBILE ) {
+			var container = $(".headshot");
 
-		container.on('mouseleave',
-			function (event) {
-				var overlay = $(event.currentTarget).children(".member-overlay");
-				overlay.css("display", "none");
-				overlay.toggleClass('red-semi-trans');
-				$(event.currentTarget).children('.cast-member-name').toggleClass('red-semi-trans');
-			}
-		);
+			container.on('mouseenter',
+				function (event) {
+					var overlay = $(event.currentTarget).children(".member-overlay");
+					overlay.css("display", "block");
+					overlay.toggleClass('red-semi-trans');
+					$(event.currentTarget).children('.cast-member-name').toggleClass('red-semi-trans');
+				}
+			);
+
+			container.on('mouseleave',
+				function (event) {
+					var overlay = $(event.currentTarget).children(".member-overlay");
+					overlay.css("display", "none");
+					overlay.toggleClass('red-semi-trans');
+					$(event.currentTarget).children('.cast-member-name').toggleClass('red-semi-trans');
+				}
+			);
+
+		}
 	};
 
-	//create instance
 	TFO.Cast = new TFO.__CastInstance();
 });
