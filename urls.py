b01e735c4f02ced99f7380e9a11b33c695e2d1ab
Keith Ensign <kensign@300feetout.com>
2014-12-15 00:07:57 -0800

    url.py needs to be tracked

--- urls.py
+++ urls.py
@@ -4,8 +4,6 @@ from django.conf.urls import patterns, include, url
 from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
 
-from mezzanine.core.views import direct_to_template
-
 
 admin.autodiscover()
 
@@ -13,57 +11,41 @@ admin.autodiscover()
 # You can also change the ``home`` view to add your own functionality
 # to the project's homepage.
 
-urlpatterns = i18n_patterns("",
-    # Change the admin prefix here to use an alternate URL for the
-    # admin interface, which would be marginally more secure.
-    ("^admin/", include(admin.site.urls)),
-)
+# Change the admin prefix here to use an alternate URL for the
+# admin interface, which would be marginally more secure.
+urlpatterns = i18n_patterns("", ("^admin/", include(admin.site.urls)),)
 
-urlpatterns += patterns('',
 
-    # We don't want to presume how your homepage works, so here are a
-    # few patterns you can use to set it up.
+# We don't want to presume how your homepage works, so here are a
+# few patterns you can use to set it up.
 
-    # HOMEPAGE AS STATIC TEMPLATE
-    # ---------------------------
-    # This pattern simply loads the index.html template. It isn't
-    # commented out like the others, so it's the default. You only need
-    # one homepage pattern, so if you use a different one, comment this
-    # one out.
+# HOMEPAGE AS STATIC TEMPLATE
+# ---------------------------
+# This pattern simply loads the index.html template. It isn't
+# commented out like the others, so it's the default. You only need
+# one homepage pattern, so if you use a different one, comment this
+# one out.
 
-    #url("^$", direct_to_template, {"template": "index.html"}, name="home"),
+# url("^$", direct_to_template, {"template": "index.html"}, name="home"),
 
-    # HOMEPAGE AS AN EDITABLE PAGE IN THE PAGE TREE
-    # ---------------------------------------------
-    # This pattern gives us a normal ``Page`` object, so that your
-    # homepage can be managed via the page tree in the admin. If you
-    # use this pattern, you'll need to create a page in the page tree,
-    # and specify its URL (in the Meta Data section) as "/", which
-    # is the value used below in the ``{"slug": "/"}`` part.
-    # Also note that the normal rule of adding a custom
-    # template per page with the template name using the page's slug
-    # doesn't apply here, since we can't have a template called
-    # "/.html" - so for this case, the template "pages/index.html"
-    # should be used if you want to customize the homepage's template.
+# HOMEPAGE AS AN EDITABLE PAGE IN THE PAGE TREE
+# ---------------------------------------------
+# This pattern gives us a normal ``Page`` object, so that your
+# homepage can be managed via the page tree in the admin. If you
+# use this pattern, you'll need to create a page in the page tree,
+# and specify its URL (in the Meta Data section) as "/", which
+# is the value used below in the ``{"slug": "/"}`` part.
+# Also note that the normal rule of adding a custom
+# template per page with the template name using the page's slug
+# doesn't apply here, since we can't have a template called
+# "/.html" - so for this case, the template "pages/index.html"
+# should be used if you want to customize the homepage's template.
 
-    url("^$", "mezzanine.pages.views.page", {"slug": "/"}, name="home"),
+urlpatterns += patterns('', url("^$", "mezzanine.pages.views.page", {"slug": "/"}, name="home"),
 
-    # HOMEPAGE FOR A BLOG-ONLY SITE
-    # -----------------------------
-    # This pattern points the homepage to the blog post listing page,
-    # and is useful for sites that are primarily blogs. If you use this
-    # pattern, you'll also need to set BLOG_SLUG = "" in your
-    # ``settings.py`` module, and delete the blog page object from the
-    # page tree in the admin if it was installed.
-
-    # url("^$", "mezzanine.blog.views.blog_post_list", name="home"),
-
-    # MEZZANINE'S URLS
-    # ----------------
-    # ADD YOUR OWN URLPATTERNS *ABOVE* THE LINE BELOW.
-    # ``mezzanine.urls`` INCLUDES A *CATCH ALL* PATTERN
-    # FOR PAGES, SO URLPATTERNS ADDED BELOW ``mezzanine.urls``
-    # WILL NEVER BE MATCHED!
+    url("^actors/(?P<slug>.*)%s$" % "/", "bbb.views.bbb.member", name="actor"),
+    url("^band/(?P<slug>.*)%s$" % "/", "bbb.views.bbb.member", name="band"),
+    url("^producers/(?P<slug>.*)%s$" % "/", "bbb.views.bbb.member", name="producer"),
 
     # If you'd like more granular control over the patterns in
     # ``mezzanine.urls``, go right ahead and take the parts you want
