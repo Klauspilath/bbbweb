a3532aadf586471a6cafbb05c84881d2d5bb8022
Keith Ensign <kensign@300feetout.com>
2014-12-14 23:57:11 -0800

    created view for cast and creative

---
+++ views/bbb.py
@@ -0,0 +1,8 @@
+__author__ = 'keithensign'
+from mezzanine.utils.views import render
+from django.template import loader
+
+
+def member(request,slug):
+    template = loader.get_template("pages/production/cast_and_crew.html",)
+    return render(request, template)
