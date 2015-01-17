51afec0ea2345d60a1943503b8b936cffeb6a509
Keith Ensign <keithensign@Keiths-iMac.local>
2015-01-16 17:22:06 -0800

    pagegrids commit

--- pagegrids/admin.py
+++ pagegrids/admin.py
@@ -1,3 +1,30 @@
+from __future__ import unicode_literals
+
 from django.contrib import admin
 
-# Register your models here.
+from mezzanine.core.admin import TabularDynamicInlineAdmin
+from mezzanine.pages.admin import PageAdmin
+from bbb.pagegrids.models import GridPage, Grid, GridRow, GridCell
+
+
+class GridInline(TabularDynamicInlineAdmin):
+    model = Grid
+
+
+class GridRowInline(TabularDynamicInlineAdmin):
+    model = GridRow
+
+
+class GridCellInline(TabularDynamicInlineAdmin):
+    model = GridCell
+
+
+class GridAdmin(PageAdmin):
+
+    class Media:
+        css = {"all": ("mezzanine/css/admin/gallery.css",)}
+
+    inlines = (GridInline,GridRowInline,GridCellInline)
+
+
+admin.site.register(GridPage, Grid)
--- pagegrids/models.py
+++ pagegrids/models.py
@@ -1,29 +1,40 @@
 from __future__ import unicode_literals
-from future.builtins import str
-
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
-from mezzanine.conf import settings
-from mezzanine.core.models import Displayable, Ownable, RichText, Orderable
+from mezzanine.core.models import RichText, Orderable
 from mezzanine.pages.models import Page
 
-
-class PageGrid(models.Model):
-    grid_title = models.CharField(_("Grid Title"), max_length=1000, blank=True)
-
+class GridPage(Page, RichText):
 
     class Meta:
-        verbose_name = _("Page Grid")
-        verbose_name_plural = _("Page Grids")
+        verbose_name = _("Grid Page")
+        verbose_name_plural = _("Grid Pages")
+
+class Grid(models.Model):
+    grid_title = models.CharField(_("Grid Title"), max_length=255, blank=True)
+    grid = models.ForeignKey("GridPage", related_name="grid_page")
+    rows = models.CharField(_("value"), max_length=4, blank=True)
+
+    class Meta:
+        verbose_name = _("Grid")
+        verbose_name_plural = _("Grids")
 
 
 class GridRow(Orderable):
-    title = models.CharField(_("value"), max_length=1000, blank=True)
-    grid = models.ForeignKey("PageGrid", related_name="page_grid")
+    title = models.CharField(_("value"), max_length=255, blank=True)
+    grid = models.ForeignKey("Grid", related_name="page_grid")
+
+    class Meta:
+        verbose_name = _("Row")
+        verbose_name_plural = _("Rows")
 
 
-class GridCell(models.Model):
-    value = models.CharField(_("value"), max_length=1000, blank=True)
+class GridCell(Orderable):
+    value = models.CharField(_("value"), max_length=20, blank=True)
     row = models.ForeignKey("GridRow", related_name="grid_row")
 
+
+    class Meta:
+        verbose_name = _("Row")
+        verbose_name_plural = _("Rows")
 
