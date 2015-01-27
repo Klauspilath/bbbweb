8f24b207ca31d61cc3cc43f37ec1138c158890b6
Keith Ensign <keithensign@Keiths-iMac.local>
2015-01-27 15:50:24 -0800

    admin working

--- ticketprices/admin.py
+++ ticketprices/admin.py
@@ -1,3 +1,23 @@
+from copy import deepcopy
 from django.contrib import admin
+from .models import Section, WeekDay, TicketPrice
 
-# Register your models here.
+
+class SectionInline(admin.ModelAdmin):
+    model = Section
+    list_display = 'name'
+
+
+class WeekDayInline(admin.ModelAdmin):
+    model = WeekDay
+    list_display = 'name'
+
+
+class TickPriceAdmin(admin.ModelAdmin):
+    section = SectionInline
+    week_day = WeekDayInline
+
+
+admin.site.register(Section)
+admin.site.register(WeekDay)
+admin.site.register(TicketPrice, TickPriceAdmin)
--- ticketprices/models.py
+++ ticketprices/models.py
@@ -1,8 +1,51 @@
+from __future__ import unicode_literals
 from django.db import models
-
-# Create your models here.
-
+from django.utils.translation import ugettext_lazy as _
 
 
-class Section(Displayable, Ownable):
-    name = models.CharField()
+class Section(models.Model):
+    name = models.CharField(max_length=40,
+                            verbose_name=_("Seating Section"),
+                            blank=True,
+                            null=False,
+                            editable=True)
+
+    def __str__(self):
+        return (self.name)
+
+    class Meta:
+        verbose_name = _("Section")
+        verbose_name_plural = _("Sections")
+        ordering = ("name",)
+
+
+class WeekDay(models.Model):
+    name = models.CharField(max_length=10,
+                            verbose_name=_("Day of Week"),
+                            blank=True,
+                            null=False,
+                            editable=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = _("Day of Week")
+        verbose_name_plural = _("Days")
+        ordering = ("name",)
+
+
+class TicketPrice(models.Model):
+    section = models.ForeignKey(Section)
+    week_day = models.ForeignKey(WeekDay)
+    price = models.CharField(max_length=4,
+                             verbose_name=_("Ticket Price"),
+                             blank=True,
+                             null=False)
+
+    class Meta:
+        verbose_name = _("Ticket Price")
+        verbose_name_plural = _("Ticket Prices")
+
+    def __str__(self):
+        return "%s -- %s -- %s" % (self.section.name, self.week_day.name, self.price)
