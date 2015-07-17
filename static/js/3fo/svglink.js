df324cb7e22e84033e2e2c2903f44706e3337118
Rex Vokey <rex@300feetout.com>
2015-07-16 22:04:17 -0400

    remove static files from git repo

--- static/js/3fo/svglink.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by keithensign on 10/8/14.
- */
-document.getElementById("svg-top").addEventListener("click", sendClickToParentDocument, false);
-
-function sendClickToParentDocument(evt)
-{
-	parent.location.href = this.ownerDocument.defaultView.frameElement.getAttribute("data-next");
-}
